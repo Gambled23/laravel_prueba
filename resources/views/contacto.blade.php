aef1176b770a3816d91c18bfb9358e3b10c219b6
César Girón <ipog71@gmail.com>
2023-09-04 16:46:40 -0600

    formulario de contacto

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+</head>
+<body>
+    <h1>Contacto</h1>
+    <form action="" method="POST">
+        <input type="text" name="nombre" placeholder="Nombre"><br>
+        <input type="email" name="email" placeholder="Email"><br>
+        <textarea name="mensaje" placeholder="Mensaje"></textarea><br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
