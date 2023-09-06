d4f29495e8aa0e35fb68a0eb5c58d06a9eb4326e
César Girón <ipog71@gmail.com>
2023-09-06 16:43:41 -0600

    creacion de migraciones

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -7,11 +7,20 @@
 </head>
 <body>
     <h1>Contacto</h1>
-    <form action="" method="POST">
+    <form action="contacto" method="POST">
+        @CSRF
         <input type="text" name="nombre" placeholder="Nombre"><br>
         <input type="email" name="email" placeholder="Email"><br>
         <textarea name="mensaje" placeholder="Mensaje"></textarea><br>
-        <input type="submit" value="Enviar">
+        <input type="submit" value="Enviar"><br>
     </form>
+    
+    @if($prueba > 2)
+        <b>Prueba es mayor a 2</b>
+    @elseif($prueba < 2)
+        <b>Prueba es menor a 2</b>
+    @else
+        <b>Prueba es 2</b>
+    @endif
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,28 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contacto', function () {
-    return view('contacto');
+#Pasar variables por la URL, a tipo se le da un valor predeterminado
+Route::get('/contacto/{tipo?}', function ($tipo = null) {
+    if ($tipo) {
+        dd($tipo);
+    }
+
+    $prueba=2;
+    return view('contacto')->with(['prueba' => $prueba]); #pasar variable tipo
 });
+
+#Si recibe datos por POST (el formulario se envió)
+#En la variable $request están los datos enviados
+Route::post('/contacto', function (Request $request) {
+    #dd($request->all());
+    #el metodo dd es un metodo que permite debugear todo lo que esté en la variable $request
+
+    return "Datos enviados correctamente";
+});
+
